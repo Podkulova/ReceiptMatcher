e44288f8472298b90892ec090ce5fcc4fb3fef85
Dagmar <152322453+Podkulova@users.noreply.github.com>
2024-06-09 09:59:13 +0200

    Přidání

---
+++ src/main/java/org/example/cookapp/CookAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.cookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cookapp/CookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
